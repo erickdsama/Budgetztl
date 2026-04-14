ad100239c25f551c67b8cd61b9f3042bfac46e0c
erick <erickdsama@gmail.com>
2026-04-14 15:34:37 -0600

    fix host

--- src/app/(auth)/actions.ts
+++ src/app/(auth)/actions.ts
@@ -7,7 +7,7 @@ import { headers } from "next/headers";
 
 async function getOrigin(): Promise<string> {
   const headersList = await headers();
-  const host = headersList.get("host") ?? "localhost:3000";
+  const host = headersList.get("host") ?? "budgetztl.novaminds.xyz";
   const protocol = headersList.get("x-forwarded-proto") ?? "http";
   return `${protocol}://${host}`;
 }
